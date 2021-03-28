b755498e0be1d24c91c8e7e754ed2e4c382e12e3
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-28 13:40:15 +0530

    [Samiksha].ADD code to enqueue the elements in the queue

---
+++ src/main/java/com/bridgelabz/datastructure/MyQueue.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.datastructure;
+
+public class MyQueue {
+
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue(){
+        this.myLinkedList = new MyLinkedList();
+    }
+
+
+    public void enqueue(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue(){
+        myLinkedList.printMyNodes();
+    }
+
+    public INode peak(){
+        return myLinkedList.head;
+    }
+}
---
+++ src/test/java/com/bridgelabz/datastructuretest/QueueTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.datastructuretest;
+
+import com.bridgelabz.datastructure.INode;
+import com.bridgelabz.datastructure.MyNode;
+import com.bridgelabz.datastructure.MyQueue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QueueTest {
+
+    @Test
+    public void given3Integers_WhenAddedToQueue_ShouldPassTheTest(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        INode peak = myQueue.peak();
+        myQueue.printQueue();
+        Assertions.assertEquals(peak, myFirstNode);
+    }
+}
